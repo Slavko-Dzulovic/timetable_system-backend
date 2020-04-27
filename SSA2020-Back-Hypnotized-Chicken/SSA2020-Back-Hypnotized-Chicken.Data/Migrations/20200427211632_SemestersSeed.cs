c520a118277bdb8d10c08444fab0336cfb9b59ba
Slavko Džulović <dzulovicslavko@gmail.com>
2020-04-27 23:46:48 +0200

    Issue #12 Add seed migrations for Semesters, Weekdays, Departments and Modules

---
+++ SSA2020-Back-Hypnotized-Chicken/SSA2020-Back-Hypnotized-Chicken.Data/Migrations/20200427211632_SemestersSeed.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using SSA2020_Back_Hypnotized_Chicken.Data.Entities;
+
+namespace SSA2020_Back_Hypnotized_Chicken.Data.Migrations
+{
+    public partial class SemestersSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql((new Semester()
+            {
+                Id = (short) Semesters.I,
+                Name = Semesters.I.ToString()
+            }).InsertSql);
+            
+            migrationBuilder.Sql((new Semester()
+            {
+                Id = (short) Semesters.II,
+                Name = Semesters.II.ToString()
+            }).InsertSql);
+            
+            migrationBuilder.Sql((new Semester()
+            {
+                Id = (short) Semesters.III,
+                Name = Semesters.III.ToString()
+            }).InsertSql);
+            
+            migrationBuilder.Sql((new Semester()
+            {
+                Id = (short) Semesters.IV,
+                Name = Semesters.IV.ToString()
+            }).InsertSql);
+            
+            migrationBuilder.Sql((new Semester()
+            {
+                Id = (short) Semesters.V,
+                Name = Semesters.V.ToString()
+            }).InsertSql);
+            
+            migrationBuilder.Sql((new Semester()
+            {
+                Id = (short) Semesters.VI,
+                Name = Semesters.VI.ToString()
+            }).InsertSql);
+            
+            migrationBuilder.Sql((new Semester()
+            {
+                Id = (short) Semesters.VII,
+                Name = Semesters.VII.ToString()
+            }).InsertSql);
+            
+            migrationBuilder.Sql((new Semester()
+            {
+                Id = (short) Semesters.VIII,
+                Name = Semesters.VIII.ToString()
+            }).InsertSql);
+            
+            migrationBuilder.Sql((new Semester()
+            {
+                Id = (short) Semesters.IX,
+                Name = Semesters.IX.ToString()
+            }).InsertSql);
+            
+            migrationBuilder.Sql((new Semester()
+            {
+                Id = (short) Semesters.X,
+                Name = Semesters.X.ToString()
+            }).InsertSql);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql((new Semester() { Id = (short)Semesters.I}).DeleteSql);
+            
+            migrationBuilder.Sql((new Semester() { Id = (short)Semesters.II}).DeleteSql);
+            
+            migrationBuilder.Sql((new Semester() { Id = (short)Semesters.III}).DeleteSql);
+            
+            migrationBuilder.Sql((new Semester() { Id = (short)Semesters.IV}).DeleteSql);
+            
+            migrationBuilder.Sql((new Semester() { Id = (short)Semesters.V}).DeleteSql);
+            
+            migrationBuilder.Sql((new Semester() { Id = (short)Semesters.VI}).DeleteSql);
+            
+            migrationBuilder.Sql((new Semester() { Id = (short)Semesters.VII}).DeleteSql);
+            
+            migrationBuilder.Sql((new Semester() { Id = (short)Semesters.VIII}).DeleteSql);
+            
+            migrationBuilder.Sql((new Semester() { Id = (short)Semesters.IX}).DeleteSql);
+            
+            migrationBuilder.Sql((new Semester() { Id = (short)Semesters.X}).DeleteSql);
+        }
+    }
+}
---
+++ SSA2020-Back-Hypnotized-Chicken/SSA2020-Back-Hypnotized-Chicken.Data/Migrations/20200427212435_ModulesSeed.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using SSA2020_Back_Hypnotized_Chicken.Data.Entities;
+
+namespace SSA2020_Back_Hypnotized_Chicken.Data.Migrations
+{
+    public partial class ModulesSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql((new Module()
+            {
+                Id = (short) Modules.SI,
+                Name = Modules.SI.ToString(),
+                DepartmentId = (short) Departments.IT
+            }).InsertSql);
+
+            migrationBuilder.Sql((new Module()
+            {
+                Id = (short) Modules.ITO,
+                Name = Modules.ITO.ToString(),
+                DepartmentId = (short) Departments.IT
+            }).InsertSql);
+
+            migrationBuilder.Sql((new Module()
+            {
+                Id = (short) Modules.ITI,
+                Name = Modules.ITI.ToString(),
+                DepartmentId = (short) Departments.IT
+            }).InsertSql);
+
+            migrationBuilder.Sql((new Module()
+            {
+                Id = (short) Modules.RI,
+                Name = Modules.RI.ToString(),
+                DepartmentId = (short) Departments.ERI
+            }).InsertSql);
+
+            migrationBuilder.Sql((new Module()
+            {
+                Id = (short) Modules.EES,
+                Name = Modules.EES.ToString(),
+                DepartmentId = (short) Departments.ERI
+            }).InsertSql);
+
+            migrationBuilder.Sql((new Module()
+            {
+                Id = (short) Modules.IEE,
+                Name = Modules.IEE.ToString(),
+                DepartmentId = (short) Departments.ERI
+            }).InsertSql);
+
+            migrationBuilder.Sql((new Module()
+            {
+                Id = (short) Modules.IM_Global,
+                Name = Modules.IM_Global.ToString(),
+                DepartmentId = (short) Departments.IM
+            }).InsertSql);
+
+            migrationBuilder.Sql((new Module()
+            {
+                Id = (short) Modules.PM_Global,
+                Name = Modules.PM_Global.ToString(),
+                DepartmentId = (short) Departments.PM
+            }).InsertSql);
+
+            migrationBuilder.Sql((new Module()
+            {
+                Id = (short) Modules.TI_Global,
+                Name = Modules.TI_Global.ToString(),
+                DepartmentId = (short) Departments.TI
+            }).InsertSql);
+
+            migrationBuilder.Sql((new Module()
+            {
+                Id = (short) Modules.MEH_Global,
+                Name = Modules.MEH_Global.ToString(),
+                DepartmentId = (short) Departments.MEH
+            }).InsertSql);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
