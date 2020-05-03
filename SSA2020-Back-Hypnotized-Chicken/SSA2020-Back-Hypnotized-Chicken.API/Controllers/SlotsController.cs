3f723e6bd1ba9d950625844c0eed92e6bea61d6f
Slavko Džulović <dzulovicslavko@gmail.com>
2020-05-03 16:26:04 +0200

    Issue #31 Move Get Subjects by module and semester from SlotsController to SubjectsController

--- SSA2020-Back-Hypnotized-Chicken/SSA2020-Back-Hypnotized-Chicken.API/Controllers/SlotsController.cs
+++ SSA2020-Back-Hypnotized-Chicken/SSA2020-Back-Hypnotized-Chicken.API/Controllers/SlotsController.cs
@@ -1,11 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using AutoMapper;
+﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
-using SSA2020_Back_Hypnotized_Chicken.API.DTOs.Slots;
-using SSA2020_Back_Hypnotized_Chicken.API.Models.Slots;
-using SSA2020_Back_Hypnotized_Chicken.Data.Entities;
 using SSA2020_Back_Hypnotized_Chicken.DataAccessLayer.UnitOfWork;
 
 namespace SSA2020_Back_Hypnotized_Chicken.API.Controllers
@@ -15,36 +9,5 @@ namespace SSA2020_Back_Hypnotized_Chicken.API.Controllers
 	public class SlotsController : BaseController<SlotsController>
 	{
 		public SlotsController(IMapper mapper, IUnitOfWork unitOfWork) : base(mapper, unitOfWork) { }
-
-		[HttpGet]
-		public async Task<ActionResult<List<SlotBySemesterAndModuleDTO>>> Subjects([FromQuery] short semesterId, short moduleId)
-		{
-			if (!ModelState.IsValid)
-			{
-				return BadRequest("Not all of the needed information is supplied.");
-			}
-			
-			var validateSemesterId = await UnitOfWork.SemestersRepository.CheckIfSemesterExistsAsync(semesterId);
-			var validateModuleId = await UnitOfWork.ModulesRepository.CheckIfModuleExistsAsync(moduleId);
-			//Task.WaitAll(validateSemesterId, validateModuleId);
-			
-			if (!validateSemesterId ||
-			    !validateModuleId)
-			{
-				return BadRequest("No semester or module by the given id exist.");
-			}
-			
-			var list = await UnitOfWork.SlotsRepository.SubjectsBySemesterAndModuleAsync(semesterId, moduleId);
-			
-			if (list == null ||
-			    !list.Any())
-			{
-				return NoContent();
-			}
-
-			var slotMapResult = Mapper.Map<List<Subject>, List<SlotBySemesterAndModuleDTO>>(list);
-
-			return Ok(slotMapResult);
-		}
 	}
 }
--- SSA2020-Back-Hypnotized-Chicken/SSA2020-Back-Hypnotized-Chicken.API/Controllers/SubjectsController.cs
+++ SSA2020-Back-Hypnotized-Chicken/SSA2020-Back-Hypnotized-Chicken.API/Controllers/SubjectsController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using SSA2020_Back_Hypnotized_Chicken.API.DTOs.Slots;
 using SSA2020_Back_Hypnotized_Chicken.API.DTOs.Subjects;
 using SSA2020_Back_Hypnotized_Chicken.Data.Entities;
 using SSA2020_Back_Hypnotized_Chicken.DataAccessLayer.UnitOfWork;
@@ -15,7 +16,7 @@ namespace SSA2020_Back_Hypnotized_Chicken.API.Controllers
 	{
 		public SubjectsController(IMapper mapper, IUnitOfWork unitOfWork) : base(mapper, unitOfWork) { }
 		
-		[HttpGet]
+		[HttpGet("all")]
 		public async Task<ActionResult<List<SubjectDTO>>> Get()
 		{
 			var subjects = await UnitOfWork.SubjectsRepository.GetSubjectsAsync();
@@ -28,6 +29,36 @@ namespace SSA2020_Back_Hypnotized_Chicken.API.Controllers
 			var subjectMapResult = Mapper.Map<List<Subject>, List<SubjectDTO>>(subjects);
 
 			return Ok(subjectMapResult);
+		}
+		
+		[HttpGet]
+		public async Task<ActionResult<List<SlotBySemesterAndModuleDTO>>> Subjects([FromQuery] short semesterId, short moduleId)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest("Not all of the needed information is supplied.");
+			}
+			
+			var validateSemesterId = await UnitOfWork.SemestersRepository.CheckIfSemesterExistsAsync(semesterId);
+			var validateModuleId = await UnitOfWork.ModulesRepository.CheckIfModuleExistsAsync(moduleId);
+			
+			if (!validateSemesterId ||
+			    !validateModuleId)
+			{
+				return BadRequest("No semester or module by the given id exist.");
+			}
+			
+			var list = await UnitOfWork.SlotsRepository.SubjectsBySemesterAndModuleAsync(semesterId, moduleId);
+			
+			if (list == null ||
+			    !list.Any())
+			{
+				return NoContent();
+			}
+
+			var slotMapResult = Mapper.Map<List<Subject>, List<SlotBySemesterAndModuleDTO>>(list);
+
+			return Ok(slotMapResult);
 		}
 	}
 }
