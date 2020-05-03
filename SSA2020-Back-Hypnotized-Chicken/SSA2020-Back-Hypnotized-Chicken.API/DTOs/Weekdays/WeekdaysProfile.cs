d8b76b0a27e5586ea54dbd7db7a5108cf3dc2a07
Slavko Džulović <dzulovicslavko@gmail.com>
2020-05-04 00:15:27 +0200

    Issue #26 Add WeekdaysController Issue #26 Add Get to WeekdaysController Issue #26 Add GetAllWeekdays methods to WeekdaysRepository Issue #26 Add WeekdayDTO and Profile

---
+++ SSA2020-Back-Hypnotized-Chicken/SSA2020-Back-Hypnotized-Chicken.API/DTOs/Weekdays/WeekdaysProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using SSA2020_Back_Hypnotized_Chicken.Data.Entities;
+
+namespace SSA2020_Back_Hypnotized_Chicken.API.DTOs.Weekdays
+{
+	public class WeekdaysProfile : Profile
+	{
+		public WeekdaysProfile()
+		{
+			CreateMap<Weekday, WeekdayDTO>()
+				.ForMember(
+					destination => destination.Id,
+					options => options.MapFrom(source => source.Id))
+				.ForMember(
+					destination => destination.Name,
+					options => options.MapFrom(source => source.Name));
+		}
+	}
+}
